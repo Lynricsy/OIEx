323f782525033e811dbbbdb09a89f501a83c63e1
Thomitics <i@foxex.cn>
2021-09-24 15:46:06 +0800

    az

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
  * Date: 2021 Sept 16
  * Algorithm:
  **************************************************************/
+#include <algorithm>
 #include <bits/stdc++.h>
 #include <cstdio>
-#include <utility>
-#include <vector>
+#include <cstring>
+#include <map>
 #define INF 0x3f3f3f3f3f3f3f3f
 #define IINF 0x3f3f3f3f
 
@@ -42,43 +43,64 @@ template <typename T> inline void write(T x, char ch = '\n', int tim = 0) {
     }
   }
 }
+const int maxN = 4e5 + 10;
 
-const long long maxN = 1090;
-long long totA;
-long long totB;
-long long totNUM;
-long long Acnt;
-vector<long long> V;
-bool usd[maxN];
-long long sum;
-long long Asum;
+int totN;
+int nums[maxN], aims[maxN];
+long long ta[maxN], tb[maxN];
+long long sa, sb;
+long long tot = 0, date[maxN], fa[maxN];
+map<long long, bool> ti;
+
+int find(int x) { return fa[x] == x ? fa[x] : fa[x] = find(fa[x]); }
+void unionn(int x, int y) { fa[x] = y; }
 
 int main() {
-  totA = read();
-  totB = read();
-  for (int i = 1; sum + i <= totA + totB; ++i) {
-    sum += i;
-    ++totNUM;
-  }
-  for (int i = totNUM; i >= 1; --i) {
-    if (Asum + i <= totA) {
-      Asum += i;
-      usd[i] = true;
-      ++Acnt;
+  totN = read();
+  for (int i = 1; i <= totN; i++)
+    nums[i] = read(), sa ^= nums[i];
+  nums[totN + 1] = sa;
+  for (int i = 1; i <= totN; i++)
+    aims[i] = read(), sb ^= aims[i];
+  aims[totN + 1] = sb;
+  totN++;
+  memcpy(ta, nums, sizeof(nums));
+  memcpy(tb, aims, sizeof(aims));
+  sort(ta + 1, ta + totN + 1);
+  sort(tb + 1, tb + totN + 1);
+  for (int i = 1; i <= totN - 1; i++)
+    if (ta[i] != tb[i])
+      return puts("-1"), 0;
+
+  int ans = 0, num = 0;
+  for (int i = 1; i <= totN; i++)
+    if (nums[i] != aims[i] || (i == totN)) {
+      date[++num] = nums[i];
+      date[++num] = aims[i];
+      if (i < totN)
+        ans++;
     }
-  }
-  write(Acnt, '\n', 1);
-  for (int i = totNUM; i >= 1; --i) {
-    if (usd[i]) {
-      write(i, ' ', 1);
+  if (ans == 0)
+    return puts("0"), 0;
+
+  sort(date + 1, date + num + 1);
+  num = unique(date + 1, date + num + 1) - date - 1;
+  for (int i = 1; i <= num; i++)
+    fa[i] = i;
+  for (int i = 1; i <= totN; i++)
+    if (nums[i] != aims[i]) {
+      nums[i] = lower_bound(date + 1, date + num + 1, nums[i]) - date,
+      aims[i] = lower_bound(date + 1, date + num + 1, aims[i]) - date;
+      if (!ti[nums[i]])
+        ti[nums[i]] = 1;
+      if (!ti[aims[i]])
+        ti[aims[i]] = 1;
+      unionn(find(nums[i]), find(aims[i]));
     }
-  }
-  putchar('\n');
-  write(totNUM - Acnt, '\n', 1);
-  for (int i = totNUM; i >= 1; --i) {
-    if (!usd[i]) {
-      write(i, ' ', 1);
-    }
-  }
+
+  for (int i = 1; i <= num; i++)
+    if (fa[i] == i)
+      ans++;
+  write(ans - 1);
   return 0;
-} // Thomitics Code
+}
